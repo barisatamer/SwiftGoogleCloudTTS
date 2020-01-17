6f0fab611dc3272b33397bf98828090bf8338e2c
Baris <baris.atamer@tillhub.de>
2020-01-17 02:15:46 +0100

    chore(package.swift): version bump google-auth-library-swift.git

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/grpc/grpc-swift.git", from: "1.0.0-alpha.6"),
         .package(url: "https://github.com/apple/swift-protobuf.git", from: "1.7.0"),
-        .package(url: "https://github.com/googleapis/google-auth-library-swift.git", from: "0.5.0")
+        .package(url: "https://github.com/googleapis/google-auth-library-swift.git", from: "0.5.1")
     ],
     targets: [
         .target(
