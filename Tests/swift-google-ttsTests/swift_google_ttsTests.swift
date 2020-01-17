eee7ae86fb0fbe89f6fe17de1e5488fd84954084
Baris <baris.atamer@tillhub.de>
2020-01-17 02:07:52 +0100

    test(listVoices): add

--- Tests/swift-google-ttsTests/swift_google_ttsTests.swift
+++ Tests/swift-google-ttsTests/swift_google_ttsTests.swift
@@ -2,14 +2,26 @@ import XCTest
 @testable import swift_google_tts
 
 final class swift_google_ttsTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(swift_google_tts().text, "Hello, World!")
+    
+    var client: GoogleTTSClient = GoogleTTSClient()
+    
+    override func setUp() {}
+
+    override func tearDown() {}
+    
+    func testListVoices() {
+        let request = Google_Cloud_Texttospeech_V1_ListVoicesRequest.with {
+            $0.languageCode = "en-US"
+        }
+        do {
+            let response = try client.listVoices(request: request).wait()
+            XCTAssert(response.voices.count == 32, "Voice count must be 32!")
+        } catch {
+            XCTFail()
+        }
     }
 
     static var allTests = [
-        ("testExample", testExample),
+        ("testListVoices", testListVoices),
     ]
 }
